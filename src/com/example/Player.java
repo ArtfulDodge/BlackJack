7fb57f71121090cf96196dabd2c3cc73c828b5b2
ArtfulDodge <31978203+ArtfulDodge@users.noreply.github.com>
2017-10-31 11:39:28 -0400

    Moved majority of Main class to Game class

--- src/com/example/Player.java
+++ src/com/example/Player.java
@@ -4,96 +4,97 @@ import java.util.*;
 
 public class Player
 {
-    private  ArrayList<Card> hand = new ArrayList<Card>(); 
+    private ArrayList<Card> hand = new ArrayList<Card>();
     public int bet = 0;
-    
-    //---------------------------------------------------------------
-    //  Default constructor
-    //---------------------------------------------------------------
+    public int money = 0;
+
+    // ---------------------------------------------------------------
+    // Default constructor
+    // ---------------------------------------------------------------
     public Player()
     {
-	
+
     }
-    
-    //---------------------------------------------------------------
-    //  Adds the top card of the deck to the Player's hand
-    //---------------------------------------------------------------
+
+    // ---------------------------------------------------------------
+    // Adds the top card of the deck to the Player's hand
+    // ---------------------------------------------------------------
     public void hit(Deck d)
     {
 	hand.add(d.draw());
     }
-    
-    //---------------------------------------------------------------
-    //  Gets the value of the player's hand based on the rules of
-    //  blackjack. Changes the values of aces from 11 to 1 
-    //  automatically to avoid busting.
-    //---------------------------------------------------------------
+
+    // ---------------------------------------------------------------
+    // Gets the value of the player's hand based on the rules of
+    // blackjack. Changes the values of aces from 11 to 1
+    // automatically to avoid busting.
+    // ---------------------------------------------------------------
     public int handValue()
     {
 	int value = 0;
 	int aces = 0;
-	for (Card i: hand)
+	for (Card i : hand)
 	{
-	    if(i.getValue() == 1)
+	    if (i.getValue() == 1)
 	    {
 		aces++;
 		value += 11;
-	    }else if(i.getValue() > 10)
+	    } else if (i.getValue() > 10)
 	    {
 		value += 10;
-	    }else
+	    } else
 	    {
 		value += i.getValue();
 	    }
-	    
-	    while(value > 21 && aces > 0)
+
+	    while (value > 21 && aces > 0)
 	    {
 		value -= 10;
 		aces -= 1;
 	    }
 	}
-	
+
 	return value;
     }
-    
-    //---------------------------------------------------------------
-    //  Returns the value of the Player's hand, minus the first 
-    //  Card
-    //---------------------------------------------------------------
+
+    // ---------------------------------------------------------------
+    // Returns the value of the Player's hand, minus the first
+    // Card
+    // ---------------------------------------------------------------
     public int showingValue()
     {
 	int value = 0;
 	int aces = 0;
-	for (Card i: hand)
+	for (Card i : hand)
 	{
-	    if(hand.indexOf(i) == 0)
+	    if (hand.indexOf(i) == 0)
 	    {
 		value += 0;
-	    }else if(i.getValue() == 1)
+	    } else if (i.getValue() == 1)
 	    {
 		aces++;
 		value += 11;
-	    }else if(i.getValue() > 10)
+	    } else if (i.getValue() > 10)
 	    {
 		value += 10;
-	    }else
+	    } else
 	    {
 		value += i.getValue();
 	    }
-	    
-	    while(value > 21 && aces > 0)
+
+	    while (value > 21 && aces > 0)
 	    {
 		value -= 10;
 		aces -= 1;
 	    }
 	}
-	
+
 	return value;
     }
-    
-    //---------------------------------------------------------------
-    //  Returns all the Cards in a Player's hand
-    //---------------------------------------------------------------
+
+    // ---------------------------------------------------------------
+    // Returns all the Cards in a Player's hand
+    // ---------------------------------------------------------------
     public String toString()
     {
 	String result = "";
@@ -102,19 +103,18 @@ public class Player
 	    if (i == 0)
 	    {
 		result += hand.get(i);
-	    }
-	    else
+	    } else
 	    {
 		result += ", " + hand.get(i);
 	    }
 	}
-	
+
 	return result;
     }
-    
-    //---------------------------------------------------------------
-    //  Returns all the Cards in the Player's hand, minus the first
-    //---------------------------------------------------------------
+
+    // ---------------------------------------------------------------
+    // Returns all the Cards in the Player's hand, minus the first
+    // ---------------------------------------------------------------
     public String showing()
     {
 	String result = "";
@@ -123,55 +123,62 @@ public class Player
 	    if (i == 0)
 	    {
 		result += "an unknown card";
-	    }
-	    else
+	    } else
 	    {
 		result += ", " + hand.get(i);
 	    }
 	}
-	
+
 	return result;
     }
-    
-    //---------------------------------------------------------------
-    //  Returns the card at the desired index in hand
-    //---------------------------------------------------------------
+
+    // ---------------------------------------------------------------
+    // Returns the card at the desired index in hand
+    // ---------------------------------------------------------------
     public Card cardInPos(int index)
     {
 	return hand.get(index);
     }
-    
-    //---------------------------------------------------------------
-    //  Returns the number of Cards in hand
-    //---------------------------------------------------------------
+
+    // ---------------------------------------------------------------
+    // Returns the number of Cards in hand
+    // ---------------------------------------------------------------
     public int cardsInHand()
     {
 	return hand.size();
     }
-    
-    //---------------------------------------------------------------
-    //  Removes the last card in the Player's hand.
-    //---------------------------------------------------------------
+
+    // ---------------------------------------------------------------
+    // Removes the last card in the Player's hand.
+    // ---------------------------------------------------------------
     public void removeCard()
     {
 	hand.remove(hand.size() - 1);
     }
-    
-    //---------------------------------------------------------------
-    //  Adds the specified Card to the Player's hand
-    //---------------------------------------------------------------
+
+    // ---------------------------------------------------------------
+    // Adds the specified Card to the Player's hand
+    // ---------------------------------------------------------------
     public void addCard(Card c)
     {
 	hand.add(c);
     }
-    
-    //---------------------------------------------------------------
-    //  Creates a card with the specified suit and value and adds 
-    //  it to the player's hand
-    //---------------------------------------------------------------
+
+    // ---------------------------------------------------------------
+    // Creates a card with the specified suit and value and adds
+    // it to the player's hand
+    // ---------------------------------------------------------------
     public void addCard(int suit, int value)
     {
 	Card c = new Card(suit, value);
 	hand.add(c);
+    }
+
+    // ---------------------------------------------------------------
+    // Clears the Player's hand
+    // ---------------------------------------------------------------
+    public void clearHand()
+    {
+	hand = new ArrayList<Card>();
     }
 }
